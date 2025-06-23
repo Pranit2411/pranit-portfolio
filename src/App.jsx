106fa029c234d5f45d0e70b2cd916893e3f9eefa
Pranit2411 <pranitmenkar24@gmail.com>
2025-06-22 21:27:01 -0400

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import Navbar from "./sections/Navbar";
+import Hero from "./sections/Hero";
+
+const App = () => {
+  return (
+    <div className='container mx-auto max-w-7xl'>
+      <Navbar />
+      <Hero />
+
+      {/* about */}
+      {/* projects */}
+      {/* experience */}
+      {/* testimonial */}
+      {/* contact */}
+      {/* footer */}
+     
+    </div>
+  );
+};
+
+export default App;
